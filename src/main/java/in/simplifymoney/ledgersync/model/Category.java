6c3156330cce7716d6bb4992c187f671e6db6fc2
Shashikumar Ramesh Naik <shashikumarnaiks82@gmail.com>
2026-09-19 11:49:55 +0530

    Update Category.java

--- src/main/java/in/simplifymoney/ledgersync/model/Category.java
+++ src/main/java/in/simplifymoney/ledgersync/model/Category.java
@@ -1,13 +1,12 @@
 package in.simplifymoney.ledgersync.model;
 
 /**
- * FROZEN - do not add, remove or rename constants.
- *
- * SPEND    money that left the user and is gone
- * INCOME   money that arrived and is theirs
- * MICRO    a small UPI spend (see the assignment for the threshold). Still
- *          spending, but reported as one rolled-up line rather than individually
- * TRANSFER a leg of the user moving their own money between their own accounts.
- *          Real, but it is neither spending nor income
+ * The four immutable transaction categories.
+ * [FROZEN CONTRACT - DO NOT MODIFY]
  */
-public enum Category { SPEND, INCOME, MICRO, TRANSFER }
+public enum Category {
+    SPEND,
+    INCOME,
+    MICRO,
+    TRANSFER
+}
